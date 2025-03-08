b47507994f63816862f6ca18ded773719ec740af
Shahariar <149128436+shahariar270@users.noreply.github.com>
2025-03-08 19:14:32 +0600

    start btn working

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-let display=document.querySelector('#show_time')
-let [hours,second,minute]=[0,0,0]
 
-const stopWatch=()=>{
+let display = document.querySelector('#show_time')
+let [hours, second, minute] = [0, 0, 0]
+let watchStart=document.querySelector('#watchStart')
+let invlidTime=null;
+const stopWatch = () => {
     second++;
-    if(second==60){
-        second=0;
-        minute++;
-    }
-    if(minute==60){
-        minute=0;
+    if (second == 60) {
+        second = 0;
+        minute++; 
+        if (minute == 60) {
+        minute = 0;
         hours++;
     }
-    display.innerHTML=`${hours}:${minute}:${second}`
+    }
+   
+    let h = hours < 10 ? '0' + hours : hours;
+    let m = minute < 10 ? '0' + minute : minute;
+    let s = second < 10 ? '0' + second : second;
+
+    display.innerHTML = `${h}:${m}:${s}`;
 }
 
-const watchStart=()=>{
-    setInterval(stopWatch,1000)
-}
-watchStart();
+watchStart.addEventListener('click',() =>{
+    if(!invlidTime){
+        setInterval(stopWatch,1000)
+    }
+})
+// watchStart();
